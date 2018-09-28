cb657734e4d14eb0ec0c62e440dc46166c585860
jace <jace.sf84@gmail.com>
2018-09-27 22:22:33 -0700

    added constructor, super props

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -16,6 +16,8 @@ let clickStatement = "Click on your favorite Family Guy character to earn points
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
   state = {
     matches,
@@ -23,14 +25,15 @@ class App extends Component {
     bestScore,
     clickMessage
 };
+  };
 
-setClicked = => {
+setClicked = id => {
   const matches = this.state.matches;
   const clickedMatch = matches.filter(match => match.id === id);
 
   // This statement says if the clicked match is true (already clicked), preform the "game-over" function. 
 
-  if(clickedMatch[0].clicked)
+  if(clickedMatch[0].clicked){
 
   correctGuesses = 0;
             clickMessage = "Dammit! You memory sucks! You clicked that already"
@@ -38,5 +41,24 @@ setClicked = => {
             matches.map(match => match.id === id);
             matches.clicked = false;
 
-}
+  
+            this.setState({clickMessage});
+            this.setState({ correctGuesses });
+            this.setState({matches});
+
+  } else if(correctGuesses < 11){
+
+  // Set its value to true
+  clickedMatch[0].clicked = true;
+
+  // increment the appropriate counter
+  correctGuesses++;
+
+  clickMessage = "Great! You haven't click on that one yet! Keep going!";
+
+  if (correctGuesses > bestScore){
+      bestScore = correctGuesses;
+      this.setState({ bestScore });
+  }
+
 export default App;

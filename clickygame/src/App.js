f0af4e7747d60fdbc95a9597f31e5b1582d39a1f
jace <jace.sf84@gmail.com>
2018-09-29 21:22:28 -0700

    added in Wrapper file and css

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import matches from "./matchcards.json";
-const Shuffle = window.Shuffle;
+// const Shuffle = window.Shuffle;
 
 
 // set varibles to store your users guesses, score, and event message (in this case a "click" event). 
@@ -14,8 +14,6 @@ let correctGuesses = 0;
 let bestScore = 0;
 let clickStatement = "Click on your favorite Family Guy character to earn points, but be sure to remember who you clicked...This is a memory game PEOPLE!";
 
-
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -24,7 +22,7 @@ class App extends Component {
     matches,
     correctGuesses,
     bestScore,
-    clickMessage
+    clickStatement
 };
   };
 
@@ -32,7 +30,6 @@ setClicked = id => {
   const matches = this.state.matches;
   const clickedMatch = matches.filter(match => match.id === id);
 
-  // This statement says if the clicked match is true (already clicked), preform the "game-over" function. 
 
   if(clickedMatch[0].clicked){
 
@@ -62,4 +59,71 @@ setClicked = id => {
       this.setState({ bestScore });
   }
 
-export default App;
+  shuffleData = data => {
+    let i = data.length - 1;
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+      i--;
+    }
+    return data;
+  };
+
+} else {
+
+  // Set its value to true
+  clickedMatch[0].clicked = true;
+
+  // restart the guess counter
+  correctGuesses = 0;
+
+  
+  clickMessage = "WOW!!! You got ALL of them!!! Now, let's see if you can do it again!";
+  bestScore = 12;
+  this.setState({ bestScore });
+  
+  for (let i = 0 ; i < matches.length ; i++){
+      matches[i].clicked = false;
+  };
+
+  shuffleData = data => {
+    let i = data.length - 1;
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = data[i];
+      data[i] = data[j];
+      data[j] = temp;
+      i--;
+    }
+    return data;
+  };
+
+  // render() {
+  //   return (
+  //       <Wrapper>
+  //           <Title>Family Guy Memory Game</Title>
+    
+  //           <h3 className="scoreSummary">
+  //               {this.state.clickMessage}
+  //           </h3>
+            
+  //           <h3 className="scoreSummary">
+  //               Correct Guesses: {this.state.correctGuesses} 
+  //               <br />
+  //               Best Score: {this.state.bestScore} 
+  //           </h3>
+
+  //           {this.state.matches.map(match => (
+  //               <MatchCard
+  //                   setClicked={this.setClicked}
+  //                   id={match.id}
+  //                   key={match.id}
+  //                   image={match.image}
+  //               />
+  //           ))}
+  //       </Wrapper>
+  // }
+  //   );
+    export default App;
